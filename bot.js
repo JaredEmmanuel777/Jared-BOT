96a4375d866b11944f7ea84a76715ec4073e9363
JaredEmmanuel777 <31585534+JaredEmmanuel777@users.noreply.github.com>
2017-09-04 20:50:40 +0800

    Update bot.js

--- bot.js
+++ bot.js
@@ -1,24 +1,27 @@
 const Discord = require("discord.js");
 const client = new Discord.Client();
-const token = '------';
+const config = require("./botsettings.json");
+const token = config.token;
+const prefix = config.prefix
 
 client.on('ready', () => {
   console.log(`Logged in as ${client.user.tag}!`);
 });
 
 client.on('guildMemberAdd', member => {
-    member.guild.defaultChannel.send(`Welcome to the server, ${member}!`);
+    member.guild.defaultChannel.send(`Welcome to the server, ${member}! Hope you have a good time here! :wink:`);
 
     const channel = member.guild.channels.find('name', 'member-log');
     if (!channel) return;
-    channel.send(`Welcome to the server, ${member}`);
+    channel.send(`Welcome to the server, ${member}! Hope you have a good time here! :wink:`);
 });
 
-
+// Messages
 client.on('message', msg => {
-  if (msg.content === 'ping') {
-    msg.reply('Pong!');
-  }
+	if (msg.author.bot) return;
+	if (msg.content === prefix + 'Ping') {
+		msg.reply('Pong!');
+	}
 });
 
 client.login(token);
