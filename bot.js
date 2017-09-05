66d287a0f5bc056d6da00d7dbcdc24861e4bc2f3
Jared Emmanuel <redmanuel777@gmail.com>
2017-09-05 21:03:54 +0800

    Update bot.js

--- bot.js
+++ bot.js
@@ -10,6 +10,11 @@ client.on('ready', () => {
 	console.log(`Logged in as ${client.user.tag}!`);
 });
 
+// Server installation alert
+client.on("guildCreate", guild => {
+	console.log('<Jared> BOT just got added to ${guild.name}, owned by ${guild.owner.user.username}!')
+});
+
 // Greets new members
 client.on('guildMemberAdd', member => {
     member.guild.defaultChannel.send(`Welcome to the server, ${member}! Hope you have a good time here! :wink:`);
@@ -19,12 +24,13 @@ client.on('guildMemberAdd', member => {
     channel.send(`Welcome to the server, ${member}! Hope you have a good time here! :wink:`);
 });
 
-// Messages
+// Messages (Ping)
 client.on('message', msg => {
 	if (msg.author.bot) return;
 	if (msg.content === prefix + 'Ping') {
 		msg.reply('Pong!');
 	}
+	console.log(msg.author.username + " just pinged the BOT " + " in " + msg.guild.name);
 });
 
 // Echo
@@ -33,7 +39,33 @@ client.on ('message', msg => {
 		let args = msg.content.split(" ").slice(1);
 		let thingToEcho = args.join(" ")
 		msg.channel.sendMessage(thingToEcho)
+		console.log(msg.author.username + " just made the BOT say " + thingToEcho + " in " + msg.guild.name);
 	}
 });
+
+// Set game
+client.on('message', msg => {
+	if (msg.content.startsWith(prefix + "setGame")) {
+		/**
+		if (msg.author.id !== "323279582952292352") {
+			return msg.reply("!!! BOT Owner only. Sorry. !!!" + msg.author)
+		}
+		**/
+		let args = msg.content.split(" ").slice(1);
+		let game = args.join(" ")
+		console.log(game)
+		client.user.setGame(game)
+	}
+});
+
+// DM
+client.on('message', msg => {
+	if (msg.content.startsWith(prefix + "dm")) {
+		let args = msg.content.split(" ").slice(1);
+		let content = args.join(" ")
+		msg.author.sendMessage(content)
+		console.log(msg.author.username + " just made the BOT DM him/her " + content + " in " + msg.guild.name);
+	}
+})
 
 client.login(token);
